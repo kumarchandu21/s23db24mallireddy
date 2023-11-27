0793b66d67a311947e7042865ae7e86055f65b27
Kumar Chandu <kumarchandu218@gmail.com>
2023-11-27 14:10:48 -0600

    added login and register

--- app.js
+++ app.js
@@ -9,23 +9,23 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
 passport.use(new LocalStrategy(
-  function(username, password, done) {
-  Account.findOne({ username: username })
-  .then(function (user){
-  if (err) { return done(err); }
-  if (!user) {
-  return done(null, false, { message: 'Incorrect username.' });
-  }
-  if (!user.validPassword(password)) {
-  return done(null, false, { message: 'Incorrect password.' });
-  }
-  return done(null, user);
+  function (username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function (err) {
+        return done(err)
+      })
   })
-  .catch(function(err){
-  return done(err)
-  })
-  })
-  )
+)
 
 
 var journey = require("./models/journey");
@@ -47,22 +47,23 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(require('express-session')({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false
-  }));
-  app.use(passport.initialize());
-  app.use(passport.session());
-  
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/journey', journeyRouter);
-app.use('/board',boardRouter);
-app.use('/choose',chooseRouter);
+app.use('/board', boardRouter);
+app.use('/choose', chooseRouter);
 app.use('/resource', resourceRouter);
 
 const connectionString = process.env.MONGO_CON;
@@ -73,7 +74,7 @@ mongoose.connect(connectionString);
 // passport config
 // Use the existing connection
 // The Account model
-var Account =require('./models/account');
+var Account = require('./models/account');
 passport.use(new LocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
@@ -83,57 +84,61 @@ var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
-
-
-async function recreateDB(){
-// Delete everything
-//await journey.deleteMany();
-
-
-await journey.deleteMany({}).maxTimeMS(30000); // Adjust the timeout value as needed
-
-let instance1 = new
-journey({destination:"Norway", seasons:'spring', cost:3000});
-instance1.save().then(doc=>{
-console.log("First object saved")}
-).catch(err=>{
-console.error(err)
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
 });
+
+
+async function recreateDB() {
+  // Delete everything
+  //await journey.deleteMany();
+
+
+  await journey.deleteMany({}).maxTimeMS(30000); // Adjust the timeout value as needed
+
+  let instance1 = new
+    journey({ destination: "Norway", seasons: 'spring', cost: 3000 });
+  instance1.save().then(doc => {
+    console.log("First object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
 }
 
 let instance2 = new
-journey({destination:"Paris", seasons:'fall', cost:5000});
-instance2.save().then(doc=>{
-console.log("Second object saved")}
-).catch(err=>{
-console.error(err)
+  journey({ destination: "Paris", seasons: 'fall', cost: 5000 });
+instance2.save().then(doc => {
+  console.log("Second object saved")
+}
+).catch(err => {
+  console.error(err)
 });
 
 let instance3 = new
-journey({destination:"Dubai", seasons:'summer', cost:6000});
-instance3.save().then(doc=>{
-console.log("Third object saved")}
-).catch(err=>{
-console.error(err)
+  journey({ destination: "Dubai", seasons: 'summer', cost: 6000 });
+instance3.save().then(doc => {
+  console.log("Third object saved")
+}
+).catch(err => {
+  console.error(err)
 });
 let reseed = true;
-if (reseed) {recreateDB();}
+if (reseed) { recreateDB(); }
 
 
 // Define a custom 404 error handler
 //app.use(function(req, res, next) {
- // res.status(404).send('Not Found');
+// res.status(404).send('Not Found');
 //});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/journey.js
+++ routes/journey.js
@@ -9,10 +9,10 @@ var router = express.Router();
 module.exports = router;*/
 
 var express = require('express');
-const journey_controlers= require('../controllers/journey');
+const journey_controlers = require('../controllers/journey');
 var router = express.Router();
 /* GET costumes */
-router.get('/', journey_controlers.journey_view_all_Page );
+router.get('/', journey_controlers.journey_view_all_Page);
 /* GET detail journey page */
 router.get('/detail', journey_controlers.journey_view_one_Page);
 /* GET create journey page */
@@ -21,11 +21,21 @@ router.get('/create', journey_controlers.journey_create_Page);
 /* GET create costume page */
 //router.get('/create', journey_controlers.journey_create_post);
 
-/* GET create update page */
+/* GET update costume page */
 router.get('/update', journey_controlers.journey_update_Page);
 
 /* GET delete journey page */
 router.get('/delete', journey_controlers.journey_delete_Page);
+
+// A little function to check if we have an authorized user and continue on
+or
+// redirect to login.
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
 
 
